b7677afacbcd853c55141569b726d969b7c936d1
lucaagius3 <160693599+lucaagius3@users.noreply.github.com>
2024-05-22 12:42:56 +1000

    Update adc.h

--- Board 1/Inc/adc.h
+++ Board 1/Inc/adc.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stm32f303xc.h>
 
-struct _ADCxPort {
+// ADC port structure containing LDR read values and initialise values
 	ADC_TypeDef *ADCx;
 	ADC_Common_TypeDef *ADCx_Common;
 	volatile uint32_t AHBENREnable;
@@ -39,40 +39,54 @@ struct _ADCxPort {
 
 typedef struct _ADCxPort ADCxPort;
 
+//ADC ports 
 extern ADCxPort ADC1_Port;
 extern ADCxPort ADC2_Port;
 extern ADCxPort ADC3_Port;
 
+//Reads an ADC to a given channel
 uint16_t ReadADC(uint32_t channel);
 
+// Reads the ADC on continuous mode, pass in an ADC port struct to use a specific ADC port
 void ContinuousReadSingleChannel(ADCxPort *ADC_port);
 
+// Reads the ADC2 on continuous mode
 void ContinuousReadSingleChannelADC2();
 
+// Reads the ADC3 on continuous mode
 void ContinuousReadSingleChannelADC3();
 
-void SingleReadMultiChannelADC();
-
+//One shot ADC1 read with multiple channels
 void SingleReadMultiChannelADC1();
 
+//One shot ADC2 read with multiple channels
 void SingleReadMultiChannelADC2();
 
+//ADC 1 and 2 interrupt handler
 void ADC1_2_IRQHandler();
 
+//ADC 3 interrupt handler
 void ADC3_IRQHandler();
 
+//Enables ADC 1 and 2 global interrupt
 void enable_ADC12_interrupt();
 
+//Enables ADC 3 global interrupt
 void enable_ADC3_interrupt();
 
+//toggles the end of conversion interrupt for a given ADC port
 void toggle_EOC_interrupt(ADCxPort *ADC_port);
 
+//toggles the ADC ready interrupt for a given ADC port
 void toggle_ADRDY_interrupt(ADCxPort *ADC_port);
 
+//toggles the end of sequence interrupt for a given ADC port
 void toggle_EOS_interrupt(ADCxPort *ADC_port);
 
+//Initiliase a given ADC
 void ADCInitialise(ADCxPort *ADC_port);
 
+//One shot ADC read for a given ADC port using interrupts and multiple channels
 void SingleReadMultiChannelADCInterrupt(ADCxPort *ADC_port);
 
 #endif
